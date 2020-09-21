604e8765cb5a4da65d7140957eb3833dd57a157b
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2020-09-21 03:42:23 +0200

    Solution of "Find correct words" apps

---
+++ permutations.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+from nltk.corpus import wordnet
+
+list_of_letters = []
+number_of_letters = int(input('How many letters are in total: '))
+
+for i in range(0, number_of_letters):
+    list_of_letters.append(input('Write char' + str(i+1) + str(": ")))
+
+no_of_perm = int(input('Write length of permutations: '))
+perm = permutations(list_of_letters, no_of_perm)
+
+new_list = []
+for i in list(perm):
+    new_list.append("".join(i))
+
+print('Possible words are listed below: \n')
+print('===================================')
+for i in range(len(new_list)):
+    if(wordnet.synsets(new_list[i])):
+        print(new_list[i])
+print('===================================')
